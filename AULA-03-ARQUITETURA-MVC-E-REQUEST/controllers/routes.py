142d8d9b1dd73ec35cc33ad094f94ecf72f59238
CAMILY RIBEIRO PEREIRA <camily.pereira@e239.local>
2026-03-30 11:10:42 -0300

    ARQUITETURA MVC

--- AULA-03-ARQUITETURA-MVC-E-REQUEST/controllers/routes.py
+++ AULA-03-ARQUITETURA-MVC-E-REQUEST/controllers/routes.py
@@ -1,8 +1,14 @@
 # IMPORTANDO RENDER_TEMPLATE
-from flask import render_template
+from flask import render_template,request,redirect,url_for
+
 
 # CRIANDO A FUNÇÃO PARA RECEBER O FLASK (APP)
 def init_app(app):
+
+    # SIMULANDO UM BD
+    listaGames = [
+        {"titulo": "CS-GO", "ano": 2012, "categoria": "FPS Online"}
+    ]
 
     # ROTA HOME
     @app.route('/')
@@ -13,7 +19,6 @@ def init_app(app):
     @app.route('/games')
     def games_page():
         
-        # Criando variáveis
         titulo = "Silk Song"
         ano = 2025
         categoria = "Metroidvania"
@@ -26,7 +31,6 @@ def init_app(app):
             "Categoria": "Metroidvania"
         }
         
-        # Enviando para o HTML
         return render_template(
             "games.html",
             titulo=titulo,
@@ -48,7 +52,6 @@ def init_app(app):
         
         jogadores = ['Eduardo', 'Ana', 'Guilherme', 'Victor', 'William']
 
-        # Vetor de consoles
         consoles_lista = ['PlayStation 5', 'Xbox Series X', 'Nintendo Switch', 'PC Gamer']
         
         return render_template(
@@ -56,4 +59,23 @@ def init_app(app):
             jogadores=jogadores,
             game=game,
             consoles=consoles_lista
+        )
+    # ROTA DE CADASTRO DE JOGOS
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+
+        # VERIFICANDO SE É POST
+        if request.method == 'POST':
+
+            listaGames.append({
+                'titulo': request.form.get('titulo'),
+                'ano': request.form.get('ano'),
+                'categoria': request.form.get('categoria')
+            })
+
+            return redirect(url_for('cadgames'))
+
+        return render_template(
+            "cadgames.html",
+            listaGames = listaGames
         )
